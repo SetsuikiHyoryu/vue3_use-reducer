8c9f1d288d3bfedea5e06f58e8b41ded8cac9925
SETSUIKI HYORYU <tayutai@outlook.jp>
2022-05-29 00:04:48 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TodoAPP from '@/views/TodoAPP/index.vue'
+</script>
+
+<template>
+  <TodoAPP />
+</template>
+
+<style>
+#app {
+  background: #888;
+}
+</style>
---
+++ src/views/TodoAPP/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+export default { name: 'TodoAPP' }
+</script>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const word = ref<string>('white thighhighs')
+</script>
+
+<template>
+  <div>{{ word }}</div>
+</template>
